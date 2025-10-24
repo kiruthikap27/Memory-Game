f22ab7a2356d7be76da52c6b2b47a6cd9074130e
kiruthikap27 <kiruthika.geeta@gmail.com>
2025-10-24 18:27:22 +0530

    card flip and reset functionality added

--- script.js
+++ script.js
@@ -41,3 +41,65 @@ function handleInitialLoad() {
     }, 100);
   });
 }
+
+function handleFlipCard(cardId) {
+  const card = document.getElementById(cardId);
+  card.classList.add("rotate");
+  card.classList.add(`flipped`);
+  const dataImage = card.getAttribute("data-image");
+  const image = card.querySelector("img");
+  image.src = dataImage;
+
+  setTimeout(() => {
+    card.classList.remove("rotate");
+  }, 100);
+
+  //check the match
+  const flippedCard = document.querySelectorAll(".flipped");
+  const previousFlippedCard = flippedCard[0];
+  const lastFlippedCard = flippedCard[1];
+
+  const removeClassName = (name) => {
+    previousFlippedCard.classList.remove(name);
+    lastFlippedCard.classList.remove(name);
+  };
+
+  const addClassName = (name) => {
+    previousFlippedCard.classList.add(name);
+    lastFlippedCard.classList.add(name);
+  };
+
+  if (flippedCard?.length > 1) {
+    removeClassName("flipped");
+    if (
+      lastFlippedCard?.dataset?.image !== previousFlippedCard?.dataset?.image
+    ) {
+      setTimeout(() => {
+        addClassName("rotate");
+        const image = previousFlippedCard.querySelector("img");
+        const lastFlippedCardImage = lastFlippedCard.querySelector("img");
+        lastFlippedCardImage.src = "assets/squareImage.png";
+        image.src = "assets/squareImage.png";
+      }, 500);
+      setTimeout(() => {
+        removeClassName("rotate");
+      }, 600);
+    } else {
+      addClassName("matched");
+
+      setTimeout(() => {
+        removeClassName("rotate");
+        if (document.querySelectorAll(".matched")?.length === 16) {
+          const header = document.querySelector("#winner");
+          header.style.display = "block";
+        }
+      }, 500);
+    }
+  }
+}
+
+function handleReset() {
+  handleInitialLoad();
+  const header = document.querySelector("#winner");
+  header.style.display = "none";
+}
